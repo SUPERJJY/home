1f9e411b6eb0f474884c92a07f04c11d30753d65
SUPERJJY <100864285+SUPERJJY@users.noreply.github.com>
2023-02-25 19:10:16 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+// Replace the following values with your own
+const username = "SUPERJJY";
+const repository = "home";
+
+// Handle form submission
+$("form").submit(function(event) {
+	event.preventDefault();
+
+	// Get form values
+	const input = $("#input-field").val();
+    const token = $("#token-field").val();
+
+	// Create data object
+	const data = {
+		name: input,
+	};
+
+	// Send data to GitHub repository using API
+	$.ajax({
+		url: `https://api.github.com/repos/${username}/${repository}/issues`,
+		type: "POST",
+		headers: {
+			Authorization: `token ${token}`
+		},
+		data: JSON.stringify(data),
+		success: function(response) {
+			// Display success message
+			alert("Thank you for submitting the questionnaire!");
+			// Reset form
+			$("form")[0].reset();
+		},
+		error: function(xhr, status, error) {
+			// Display error message
+			alert(`An error occurred: ${error}`);
+		}
+	});
+});
